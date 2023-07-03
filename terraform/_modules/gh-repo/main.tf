107edd704f0ba5109e8441f588c65af4223c5dc5
Luke Browning <browningluke01@gmail.com>
2023-07-03 04:26:46 -0700

    Use defaults from vars

--- terraform/_modules/gh-repo/main.tf
+++ terraform/_modules/gh-repo/main.tf
@@ -1,40 +1,37 @@
-locals {
-  github_conf = yamldecode(var.repo_config)
-  repos       = { for each in local.github_conf : each.name => each }
-}
-
 resource "github_repository" "repo" {
-  for_each = local.repos
+  for_each = {
+    for repo in var.repo_config : repo.name => repo
+  }
 
   # Meta
   name        = each.key
-  description = lookup(each.value, "description", "")
-  visibility  = lookup(each.value, "public", false) ? "public" : "private"
-  is_template = lookup(each.value, "is_template", false)
+  description = each.value.description
+  visibility  = each.value.public ? "public" : "private"
+  is_template = each.value.is_template
 
-  archived = lookup(each.value, "archived", false)
+  archived = each.value.archived
 
   # Has ...
-  has_downloads = can(each.value.has.downloads) ? each.value.has.downloads : true
-  has_issues    = can(each.value.has.issues) ? each.value.has.issues : true
-  has_projects  = can(each.value.has.projects) ? each.value.has.projects : true
-  has_wiki      = can(each.value.has.wiki) ? each.value.has.wiki : true
+  has_downloads = each.value.has.downloads
+  has_issues    = each.value.has.issues
+  has_projects  = each.value.has.projects
+  has_wiki      = each.value.has.wiki
 
   # Init
-  auto_init          = lookup(each.value, "init", false)
-  gitignore_template = lookup(each.value, "gitignore", "")
+  auto_init          = each.value.init
+  gitignore_template = each.value.gitignore
 
   # Merges
-  allow_auto_merge       = can(each.value.merge.auto) ? each.value.merge.auto : false
-  delete_branch_on_merge = can(each.value.merge.delete_branch) ? each.value.merge.delete_branch : false
+  allow_auto_merge       = each.value.merge.auto
+  delete_branch_on_merge = each.value.merge.delete_branch
 
-  allow_merge_commit = can(each.value.merge.types.commit) ? each.value.merge.types.commit : true
-  allow_squash_merge = can(each.value.merge.types.squash) ? each.value.merge.types.squash : true
-  allow_rebase_merge = can(each.value.merge.types.rebase) ? each.value.merge.types.rebase : true
+  allow_merge_commit = each.value.merge.types.commit
+  allow_squash_merge = each.value.merge.types.squash
+  allow_rebase_merge = each.value.merge.types.rebase
 
   # Template
   dynamic "template" {
-    for_each = can(each.value.template.owner) && can(each.value.template.repo) ? [each.value.template] : []
+    for_each = each.value.template != null ? [each.value.template] : []
 
     content {
       owner                = template.value.owner
