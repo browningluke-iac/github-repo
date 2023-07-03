1e25a76ee46966f3483b7a7d3d8fc570c81447f8
Luke Browning <browningluke01@gmail.com>
2023-07-03 04:23:47 -0700

    Switch browningluke-iac over to local module

--- terraform/browningluke-iac/repo.tf
+++ terraform/browningluke-iac/repo.tf
@@ -1,6 +1,5 @@
 module "github_repos" {
-  source  = "app.terraform.io/browningluke/repo/github"
-  version = "~> 1.0"
+  source = "../_modules/gh-repo"
 
   repo_config = file("${path.module}/../../data/browningluke-iac.yml")
 }
